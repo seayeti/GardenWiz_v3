199ca2d19f5ea69015ff0459e48d072acdb0ca0c
rich <Richard>
2022-05-01 19:04:10 -0700

    added JWT to login and MainActivity

--- app/src/main/java/com/example/gardenwiz_v3/LoginPage.java
+++ app/src/main/java/com/example/gardenwiz_v3/LoginPage.java
@@ -1,96 +1,3 @@
-//package com.example.gardenwiz_v3;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.TextView;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import com.example.gardenwizv2.MainActivity;
-//import com.example.gardenwizv2.R;
-//
-////import API.RetrofitBuilder;
-////import API.login;
-////import API.userApi;
-////import retrofit2.Call;
-////import retrofit2.Callback;
-////import retrofit2.Response;
-////import retrofit2.Retrofit;
-//
-//public class LoginPage extends AppCompatActivity {
-//
-//    private EditText Name;
-//    private EditText Password;
-//    private TextView Info;
-//    private Button Login;
-//    private int counter = 5;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_login_page);
-//
-//        Name = (EditText) findViewById(R.id.etName);
-//        Password = (EditText) findViewById(R.id.etPassword);
-//        Info = (TextView) findViewById(R.id.tvInfo);
-//        Login = (Button) findViewById(R.id.btnLogin);
-//
-//        //Automatic set text to easily bypass login
-//        Name.setText("a2@gmail.com");
-//        Password.setText("555");
-//
-//        Info.setText("No of attempts remaining: 5");
-//
-//        Login.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                System.out.println();
-//                Retrofit retrofit = RetrofitBuilder.getInstance();
-//                userApi myuserAPI = retrofit.create(userApi.class);
-//                Call<login> call = myuserAPI.login(Name.getText().toString(),Password.getText().toString());
-//                call.enqueue(new Callback<login>() {
-//                    @Override
-//                    public void onResponse(Call<login> call, Response<login> response) {
-//                        if(response.body() != null) {
-//                            validate(response.body().getMessage());
-//                        }else{
-//                            counter--;
-//
-//                            Info.setText("No of attempts remaining: " + String.valueOf(counter));
-//
-//                            if(counter == 0){
-//                                Login.setEnabled(false);
-//                            }
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<login> call, Throwable t) {
-//
-//                    }
-//                });
-//
-//
-//            }
-//        });
-//
-//    }
-//
-//    private void validate(String Loginmessage) {
-//        if(Loginmessage.equals("Successful login.")){
-//            Intent intent = new Intent(LoginPage.this, MainActivity.class);
-//            startActivity(intent);
-//        }else{
-//
-//        }
-//    }
-//
-//}
-
-//Without Retrofit
 package com.example.gardenwiz_v3;
 
 import android.content.Intent;
@@ -101,6 +8,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gardenwiz_v3.MainActivity;
+import com.example.gardenwiz_v3.R;
+
+import API.RetrofitBuilder;
+import API.login;
+import API.userApi;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -121,18 +39,44 @@ public class LoginPage extends AppCompatActivity {
         Info = (TextView) findViewById(R.id.tvInfo);
         Login = (Button) findViewById(R.id.btnLogin);
         Register = (Button) findViewById(R.id.btnReg);
-
+        //Automatic set text to easily bypass login
+        Name.setText("a2@gmail.com");
+        Password.setText("555");
 
         Info.setText("No of attempts remaining: 5");
-        Name.setText("Admin");
-        Password.setText("1234");
+
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validate(Name.getText().toString(), Password.getText().toString());
+                System.out.println();
+                Retrofit retrofit = RetrofitBuilder.getInstance();
+                userApi myuserAPI = retrofit.create(userApi.class);
+                Call<login> call = myuserAPI.login(Name.getText().toString(),Password.getText().toString());
+                call.enqueue(new Callback<login>() {
+                    @Override
+                    public void onResponse(Call<login> call, Response<login> response) {
+                        if(response.body() != null) {
+                            validate(response.body().getMessage(),response.body().getJwt());
+                        }else{
+                            counter--;
+
+                            Info.setText("No of attempts remaining: " + String.valueOf(counter));
+
+                            if(counter == 0){
+                                Login.setEnabled(false);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<login> call, Throwable t) {
+
+                    }
+                });
+
+
             }
         });
-
         Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -143,19 +87,87 @@ public class LoginPage extends AppCompatActivity {
 
     }
 
-    private void validate(String userName, String userPassword) {
-        if((userName.equals("Admin")) && (userPassword.equals("1234"))){
+    private void validate(String Loginmessage, String JWT) {
+        if(Loginmessage.equals("Successful login.")){
+            Bundle bundle = new Bundle();
+            System.out.println("Login " + JWT);
+            bundle.putString("JWT", JWT);
             Intent intent = new Intent(LoginPage.this, MainActivity.class);
+            intent.putExtras(bundle);
             startActivity(intent);
         }else{
-            counter--;
 
-            Info.setText("No of attempts remaining: " + String.valueOf(counter));
-
-            if(counter == 0){
-                Login.setEnabled(false);
-            }
         }
     }
 
 }
+
+//Without Retrofit
+//package com.example.gardenwiz_v3;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class LoginPage extends AppCompatActivity {
+//
+//    private EditText Name;
+//    private EditText Password;
+//    private TextView Info;
+//    private Button Login;
+//    private Button Register;
+//    private int counter = 5;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login_page);
+//
+//        Name = (EditText) findViewById(R.id.etName);
+//        Password = (EditText) findViewById(R.id.etPassword);
+//        Info = (TextView) findViewById(R.id.tvInfo);
+//        Login = (Button) findViewById(R.id.btnLogin);
+//        Register = (Button) findViewById(R.id.btnReg);
+//
+//
+//        Info.setText("No of attempts remaining: 5");
+//        Name.setText("Admin");
+//        Password.setText("1234");
+//        Login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                validate(Name.getText().toString(), Password.getText().toString());
+//            }
+//        });
+//
+//        Register.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(LoginPage.this, RegistrationPage.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//    }
+//
+//    private void validate(String userName, String userPassword) {
+//        if((userName.equals("Admin")) && (userPassword.equals("1234"))){
+//            Intent intent = new Intent(LoginPage.this, MainActivity.class);
+//            startActivity(intent);
+//        }else{
+//            counter--;
+//
+//            Info.setText("No of attempts remaining: " + String.valueOf(counter));
+//
+//            if(counter == 0){
+//                Login.setEnabled(false);
+//            }
+//        }
+//    }
+//
+//}
--- app/src/main/java/com/example/gardenwiz_v3/MainActivity.java
+++ app/src/main/java/com/example/gardenwiz_v3/MainActivity.java
@@ -8,14 +8,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button runPi, search, history;
+    private  String JWT = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        Bundle bundle = getIntent().getExtras();
+        JWT = bundle.getString("JWT");
         runPi = (Button) findViewById(R.id.pi_go);
         search = (Button) findViewById(R.id.search);
         history = (Button) findViewById(R.id.history);
@@ -41,15 +42,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openPiGo(){
+        Bundle bundle = new Bundle();
+        bundle.putString("JWT", JWT);
         Intent intent = new Intent(this, RunWiz.class);
+        intent.putExtras(bundle);
         startActivity(intent);
     }
     public void openSearchPlants(){
+        Bundle bundle = new Bundle();
+        bundle.putString("JWT", JWT);
         Intent intent = new Intent(this,SearchPlants.class);
+        intent.putExtras(bundle);
         startActivity(intent);
     }
     public void openHistory(){
+        Bundle bundle = new Bundle();
+        bundle.putString("JWT", JWT);
         Intent intent = new Intent(this,History.class);
+        intent.putExtras(bundle);
         startActivity(intent);
     }
 
